2233f8d3c64172bfc013b842de8389c0d93e2906
Stephen Olayemi <stephen.olayemi@innotelligent.com>
2021-09-02 19:14:38 +0100

    home contenet 2 designs

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,15 @@ import { Container, Row, Col } from "react-bootstrap";
 import NavBar from "./NavBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
-
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('response not ignored!');
+   };
+
     return ( 
         <div className="home-page ">
            <div className="home-content1">
@@ -20,20 +26,39 @@ const Home = () => {
                <Container>
                   <Row className="justify-content-center ">
                      <Col md={5}>
-                        <h3>Enter your details</h3>
-                        <div className="form-group">
-                           <label className="titleLabels">Username:</label>
-                           <input className="form-control" type="text"/>
-                        </div>
-                        <div className="form-group">
-                           <label className="titleLabels">Password:</label>
-                           <input className="form-control" type="password"/>
-                        </div>
+                        <form onSubmit={handleSubmit}>
+                           <h3>Enter your details</h3>
+                           <div className="form-group">
+                              <label className="titleLabels">Username:</label>
+                              <input style={{border:0}}
+                                     required
+                                     className="fcnt"
+                                     type="text"/>
+                           </div>
+                           <div className="form-group">
+                              <label className="titleLabels">Password:</label>
+                              <input style={{border:0}}
+                                     required 
+                                     className="fcnt" 
+                                     type="password"/>
+                           </div>
+                           <div className="form-group">
+                              <button type="submit" className="bltn"><FontAwesomeIcon icon={faCheckCircle}/> Check In</button>
+                           </div>
+                        </form>
                      </Col>
                   </Row>
                </Container>
            </div>
-           
+           <div className="home-content3">
+               <Container>
+                  <Row className="justify-content-center ">
+                     <Col md={5}>
+                       
+                     </Col>
+                  </Row>
+               </Container>
+           </div>
         </div>
      );
 }
